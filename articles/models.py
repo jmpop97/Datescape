428c7a6956f3edcebb254b53b8ccbaa0d5897b7a
Lee Hyeonsik <hyeon5819@gmail.com>
2023-06-05 21:15:18 +0900

    feat: comment 관련 모델 추가

--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from users.models import CommonModel
+from emoticons.models import EmoticonImage
 
 # Create your models here.
 class Article(CommonModel):
@@ -37,3 +38,25 @@ class TagList(CommonModel):
     def __str__(self):
         return self.articles
 
+class Comment(CommonModel):
+    """
+    댓글 모델입니다.
+    게시글 객체와 일대다 관계를 가집니다.
+    Comment객체는 게시글 객체를 의미하는 article필드, 댓글 작성자를 의미하는 writer필드,
+    댓글내용의 comment필드, 사용된 이모티콘을 의미하는 use_emoticon필드로 구성됩니다.
+    """
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    # writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField("댓글 내용", blank=True)
+    use_emoticon = models.ForeignKey(EmoticonImage, on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return self.comment
+        # user연결되면 writer로 변경?
+
+class CommentLike(CommonModel):
+    # likers = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.comment
