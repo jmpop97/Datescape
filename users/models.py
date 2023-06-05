4ef7a0f49efd5e60c5c890b279ed9c5e0de79dda
jmpop <pjm970128@gmail.com>
2023-06-05 18:03:37 +0900

    feat: common model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class CommonModel(models.Model):
+    db_status_choice = [
+        (1, 'active'),
+        (2, 'delete'),
+    ]
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    db_status = models.PositiveIntegerField(
+        choices=db_status_choice, default=1)
+
 # Create your models here.
