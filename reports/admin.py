f7383baf99fd2fa8f80b1d9084bdb34c89bda49f
jmpop <pjm970128@gmail.com>
2023-06-06 23:56:10 +0900

    fix: did'nt commit

--- reports/admin.py
+++ reports/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from reports.models import (ReportUser,ReportArticle,ReportComment)
+from users.admin import CommonModelAdmin
 
 # Register your models here.
+class ReportUserAdmin(CommonModelAdmin):
+    fields=("reporter","report_user")
+    list_display=("reporter","report_user")
+    readonly_fields=("reporter","report_user")
+
+
+class ReportArticleAdmin(CommonModelAdmin):
+    fields=("reporter","report_article")
+    list_display=("reporter","report_article")
+    readonly_fields=("reporter","report_article")
+
+
+class ReportCommentAdmin(CommonModelAdmin):
+    fields=("reporter","report_comment")
+    list_display=("reporter","report_comment")
+    readonly_fields=("reporter","report_comment")
+
+
+admin.site.register(ReportUser, ReportUserAdmin)
+admin.site.register(ReportArticle, ReportArticleAdmin)
+admin.site.register(ReportComment, ReportCommentAdmin)
