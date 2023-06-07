28cea4f448ac1933a9325b962727ef61f40b7b33
Lee Hyeonsik <hyeon5819@gmail.com>
2023-06-07 10:13:51 +0900

    feat: (이모티콘) 이모티콘 어드민에 공통 어드민 적용

--- emoticons/admin.py
+++ emoticons/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 from emoticons.models import Emoticon, EmoticonImage, UserEmoticonList
+from users.admin import CommonModelAdmin
+
 
 # Register your models here.
-admin.site.register(Emoticon)
-admin.site.register(EmoticonImage)
-admin.site.register(UserEmoticonList)
+class EmoticonAdmin(CommonModelAdmin):
+    fields = ("creator", "title")
+    list_display = ("creator", "title")
+    readonly_fields = ("creator", "title")
+
+
+class EmoticonImageAdmin(CommonModelAdmin):
+    fields = ("emoticon", "image")
+    list_display = ("emoticon", "image")
+    readonly_fields = ("emoticon", "image")
+
+
+class UserEmoticonListAdmin(CommonModelAdmin):
+    fields = ("buyer", "sold_emoticon")
+    list_display = ("buyer", "sold_emoticon")
+    readonly_fields = ("buyer", "sold_emoticon")
+
+
+admin.site.register(Emoticon, EmoticonAdmin)
+admin.site.register(EmoticonImage, EmoticonImageAdmin)
+admin.site.register(UserEmoticonList, UserEmoticonListAdmin)
