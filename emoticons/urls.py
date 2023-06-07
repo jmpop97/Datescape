379414f7014b8339bcd554f93d7d926a6221b706
Lee Hyeonsik <hyeon5819@gmail.com>
2023-06-07 10:11:38 +0900

    feat: (이모티콘 기능) 임시저장상태 이모티콘 조회추가

--- emoticons/urls.py
+++ emoticons/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.EmoticonView.as_view(), name="emoticon"),                                # 구매한 이모티콘 조회 / 제작 신청 / 수정 / 삭제
     path('list/', views.EmoticonListView.as_view(), name="emoticon_list"),                  # 전체 이모티콘 조회
     path('<int:emoticon_id>/', views.EmoticonDetailView.as_view(), name="emoticon_detail"), # 이모티콘 자세히 보기
+    path('temp/', views.EmoticonTempListView.as_view(), name="emoticon_temp_list"),         # 임시저장 이모티콘 리스트
 ]
