298b36a24efb7bb1fd393d729e1d26ac81bc6373
Choi Jinkyu <kyuparfum@naver.com>
2023-06-09 20:34:14 +0900

    feat: 태그 MtoMField 중간모델 작성 후 태그 기능구현

--- articles/admin.py
+++ articles/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from articles.models import (Article, Tag, 
+from articles.models import (Article, Tag, TagList,
                              Comment, CommentLike, 
                              KakaoMapDataBase)
 from users.admin import CommonModelAdmin
@@ -7,23 +7,35 @@ from users.admin import CommonModelAdmin
 
 # Register your models here.
 admin.site.register(Tag)
-# admin.site.register(Article)
 admin.site.register(KakaoMapDataBase)
+
+class TagListInline(admin.TabularInline):
+    model = TagList
+
+
+class TagListAdmin(admin.ModelAdmin):
+    list_display = ["id", "tag","article"]
+    list_display_links = ["id", "tag","article"]
+    search_fields = ["id", "tag","article"]
 
 
 class ArticlesAdmin(admin.ModelAdmin):
-
     list_display = ["id", "title", "content", "score"]
     list_filter = ["title",]
     fieldsets = []
-
+    inlines = [
+        TagListInline
+    ]
     search_fields = ["title", "content",]
     ordering = ["title"]
     filter_horizontal = []
     list_display_links = ["id", "title", "content",]
+    extra = 0
 
 
+admin.site.register(TagList, TagListAdmin)
 admin.site.register(Article, ArticlesAdmin)
+
 
 class CommentAdmin(CommonModelAdmin):
     fields = ("article", "writer", "comment", "use_emoticon")
