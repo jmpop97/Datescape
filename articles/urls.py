5ad6760264802a76df848c880602f9f6fe6ec63c
heyrin1998 <qq5200@naver.com>
2023-06-09 20:07:53 +0900

    feat: 검색, 페이지네이션 추가

--- articles/urls.py
+++ articles/urls.py
@@ -4,9 +4,10 @@ from articles import views
 
 urlpatterns = [
     path('', views.ArticleView.as_view(), name='article_list'),
-    path('location-list/', views.ArticleLocationView.as_view(), name='location_list'),
+    path('location-list', views.ArticleLocationView.as_view(), name='location_list'),
     path('<int:article_id>', views.ArticleDetailView.as_view(), name='article_detail'),
     path('coordinate', views.KakaoMapCoordinateView.as_view(), name='coordinate_map'),
+    path('article-search/', views.ArticleSearchView.as_view(), name='article_search'),
     path('search', views.KakaoMapSearchView.as_view(), name='search_map'),
     path('map-save', views.KakaoSaveView.as_view(), name='save_map'),
     path('<int:article_id>/comments/', views.CommentView.as_view(), name='comment_view'),   # 댓글 생성 / 조회 / 수정 / 삭제
--- dsproject/settings.py
+++ dsproject/settings.py
@@ -108,7 +108,9 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
 }
 
 AUTH_USER_MODEL = "users.User"
