ae797ed2c779e3352de835521432977ba9217b76
jmpop <pjm970128@gmail.com>
2023-06-15 03:04:10 +0900

    refactor: simple report

--- reports/views.py
+++ reports/views.py
@@ -26,62 +26,29 @@ class ReportView(APIView):
         해당 report DB에 저장
     """
 
-    def report_user(self, request):
-        """request_type:   "user" """
-        serializer = ReportUserSerializer(data=request.data)
-        print(request.user)
-        if serializer.is_valid():
-            serializer.save(reporter=request.user.id)
-            status_is = status.HTTP_200_OK
-            message_is = {"message": "저장완료"}
-        else:
-            status_is = status.HTTP_400_BAD_REQUEST
-            message_is = serializer.errors
-        return message_is, status_is
-
-    def report_article(self, request):
-        """request_type:   "article" """
-        serializer = ReportArticleSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(reporter=request.user.id)
-            status_is = status.HTTP_200_OK
-            message_is = {"message": "저장완료"}
-        else:
-            status_is = status.HTTP_400_BAD_REQUEST
-            message_is = serializer.errors
-        return message_is, status_is
-
-    def report_comment(self, request):
-        """request_type:   "comment" """
-        serializer = ReportCommentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(reporter=request.user.id)
-            status_is = status.HTTP_200_OK
-            message_is = {"message": "저장완료"}
-        else:
-            status_is = status.HTTP_400_BAD_REQUEST
-            message_is = serializer.errors
-        return message_is, status_is
-
-    def fail(self, request):
-        """request_type:  errors"""
-        status_is = status.HTTP_400_BAD_REQUEST
-        message_is = {"message": "신고유형이 잘못되었습니다."}
-        return message_is, status_is
-
     request_dic = {
-        "user": report_user,
-        "article": report_article,
-        "comment": report_comment,
-        "fail": fail,
+        "user": ReportUserSerializer,
+        "article": ReportArticleSerializer,
+        "comment": ReportCommentSerializer,
     }
 
     def post(self, request):
         request_type = request.data.get("request_type")
-        message_is, status_is = self.request_dic.get(
-            request_type, self.request_dic["fail"]
-        )(self, request)
-        return Response(message_is, status=status_is)
+        try:
+            serializer = self.request_dic[request_type](data=request.data)
+            if serializer:
+                if serializer.is_valid():
+                    serializer.save(reporter=request.user.id)
+                    status_is = status.HTTP_200_OK
+                    message_is = {"message": "저장완료"}
+                else:
+                    status_is = status.HTTP_400_BAD_REQUEST
+                    message_is = serializer.errors
+                return Response(message_is, status=status_is)
+        except:
+            return Response(
+                {"message": "신고유형이 잘못되었습니다."}, status=status.HTTP_400_BAD_REQUEST
+            )
 
 
 class CategoryView(APIView):
